e551e7eb998c37dcbfcee514c214b616cb7a0d50
nastradamus39 <a.v.krizhanovsky@k50dev.ru>
2022-07-13 19:06:21 +0300

    регистрация, аунтефикация, авторизация, загрузка заказа

--- gophermart/app.go
+++ gophermart/app.go
@@ -1,5 +1,10 @@
 package gophermart
 
+import (
+	"github.com/gorilla/sessions"
+	"github.com/jmoiron/sqlx"
+)
+
 // Конфигурирование сервиса накопительной системы лояльности
 // Сервис должен поддерживать конфигурирование следующими методами:
 // адрес и порт запуска сервиса: переменная окружения ОС RUN_ADDRESS или флаг -a;
@@ -8,6 +13,12 @@ package gophermart
 
 // Cfg конфиг приложения
 var Cfg Config
+
+// DB подключение к базе
+var DB *sqlx.DB
+
+// SessionStore хранилище сессий
+var SessionStore = sessions.NewCookieStore([]byte("aaa"))
 
 // Config конфиг приложения
 type Config struct {
---
+++ internal/middlewares/auth.go
@@ -0,0 +1,35 @@
+package middlewares
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/nastradamus39/gophermart/gophermart"
+	"github.com/nastradamus39/gophermart/internal/db"
+	"github.com/nastradamus39/gophermart/internal/handlers"
+)
+
+func UserAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// сессия текущего пользователя
+		session, err := gophermart.SessionStore.Get(r, "go-session")
+
+		userLogin, ok := session.Values["userId"].(string)
+
+		if !ok {
+			handlers.UnauthorizedResponse(w, r)
+			return
+		}
+
+		// ищем пользователя в базе
+		user, err := db.Repositories().Users.Find(userLogin)
+
+		if err != nil {
+			handlers.UnauthorizedResponse(w, r)
+			return
+		}
+
+		// в контекст передаем ссылку на пользователя
+		next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), "user", &user)))
+	})
+}
